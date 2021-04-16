93ac3bbcd4785073813c9dc03114a02dd5cb08cb
Sam Laubscher <hakaewt@protonmail.com>
2021-04-16 23:30:33 +0100

    Add productdata file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import classes from './App.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className={classes.Topbar}>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seeklogo.net%2Fwp-content%2Fuploads%2F2016%2F10%2Famazon-logo-preview.png&f=1&nofb=1" alt="logo"/>
+        </nav>
       </header>
     </div>
   );
